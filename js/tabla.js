0c915db2fc829c7051e4dee916e16b096f16ec22
Readbook04 <123922738+Readbook04@users.noreply.github.com>
2023-03-07 19:58:57 -0600

    Delete tabla.js

--- js/tabla.js
+++
@@ -1,9 +0,0 @@
-const selectTabla = document.getElementById('Seleccion');
-selectTabla.addEventListener('change', () => {
-  const tablaId = selectTabla.value;
-  const tabla = document.querySelector(`#${tablaId}`);
-  document.querySelectorAll('table').forEach((t) => {
-    t.style.display = 'none';
-  });
-  tabla.style.display = 'block';
-});
