e45d548268b50fe3d8fa6f85b19fd2d2d682bd4f
Readbook04 <123922738+Readbook04@users.noreply.github.com>
2023-04-11 00:04:24 -0600

    Delete js directory

--- js/menu.js
+++
@@ -1,5 +0,0 @@
-const navegar = document.querySelector('.navegador');
-
-window.addEventListener('scroll', function(){
-    navegar.classList.toggle('activar', window.scrollY>0)
-});
--- js/opciones.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('.opciones').addEventListener('click', function() {
-    document.querySelector('.des-opciones').style.display = 'block';
-});
-
-document.addEventListener('click', function(e) {
-    if (!document.querySelector('.des-opciones').contains(e.target) && e.target != document.querySelector('.opciones')) {
-        document.querySelector('.des-opciones').style.display = 'none';
-    }
-});
