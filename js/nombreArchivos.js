ed2da36262d4093fb3683957d4f310c46e9f9441
Readbook04 <123922738+Readbook04@users.noreply.github.com>
2023-04-12 23:57:45 -0600

    Delete js directory

--- js/nombreArchivos.js
+++
@@ -1,7 +0,0 @@
-var fileInput = document.getElementById('fileInput');
-var fileName = document.getElementById('fileName');
-
-fileInput.addEventListener('change', function() {
-  var archivo = fileInput.files[0];
-  fileName.textContent = archivo.name;
-});
